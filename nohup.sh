d42506752d413a38167e652e5415bdefc00da037
wejd <wejd.daghfous@gmail.com>
2019-03-31 15:25:59 +0200

    file change on  dimanche 31 mars 2019, 15:25:59 (UTC+0200)

--- nohup.sh
+++ nohup.sh
@@ -1,1 +1,1 @@
-inotifywait -m -e modify -e create -e delete -e close_write /home/wejd/Bureau/gitAutomaticPush | /home/wejd/Bureau/gitAutomaticPush/script.sh
+inotifywait -m -e modify -e create -e delete -e close_write -r -o /home/wejd/Bureau/Bureau/gitAutomaticPush/nohup.out -d /home/wejd/Bureau/gitAutomaticPush | /home/wejd/Bureau/gitAutomaticPush/script.sh
